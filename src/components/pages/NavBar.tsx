aacc68fe0660b3c69165b2a4da6d2def4994dcd6
menachem <AM0556727820@GMAIL.COM>
2024-11-04 11:37:09 +0200

    + log out

--- src/components/pages/NavBar.tsx
+++ src/components/pages/NavBar.tsx
@@ -1,11 +1,22 @@
-import { NavLink } from "react-router-dom";
+import { useNavigate, NavLink } from "react-router-dom";
 
 export default function NavBar() {
+  const navigate = useNavigate();
+
   return (
     <div className="navleft">
       <NavLink to={"/pages/home"}>home</NavLink>
       <NavLink to={"/pages/credit"}>credit</NavLink>
       <NavLink to={"/pages/Transaction"}>Transaction</NavLink>
+      <button
+        onClick={() => {
+          alert("Cd");
+          localStorage.setItem("isLogin", "");
+          navigate("/auth"); // כאן השתמש ב-navigate
+        }}
+      >
+        Log Out
+      </button>
     </div>
   );
 }
