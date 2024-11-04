d98781973911ea93d2c0addd7a2a50b1da976662
menachem <AM0556727820@GMAIL.COM>
2024-11-04 12:37:59 +0200

    נסייון לpage

--- src/context/account.tsx
+++ src/context/account.tsx
@@ -1,11 +1,6 @@
 import { createContext } from "react";
 import Account from "../types/account";
 
-const AccountContext = createContext<Account>({
-    balance:5000,
-    id:'12345',
-    loans:[],
-    transactions:[]
-})
+const AccountContext = createContext<Account | undefined>(undefined)
 
 export default AccountContext
--- src/context/bank.tsx
+++ src/context/bank.tsx
@@ -1,12 +1,6 @@
 import { createContext } from "react";
 import Bank from "../types/bank";
 
-const BankContext = createContext<Bank>({
-    treasure:10000,
-    users:[
-        {name:"yo",account_id:'12345'},
-        {name:"yo",account_id:'54321'}
-    ]
-})
+const BankContext = createContext<Bank | undefined>(undefined)
 
 export default BankContext
